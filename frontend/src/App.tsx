f521fb6e6ff10ae341e80d280b6973cdde044387
anominos <anominosgamer@gmail.com>
2026-07-20 16:33:38 +0100

    formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import Board from './components/Board.tsx'
+import Board from "./components/Board.tsx";
 
 export default function App() {
   return (
     <main className="min-h-screen bg-slate-100 text-slate-900 dark:bg-slate-950 dark:text-slate-100 flex items-center justify-center p-4">
       <Board />
     </main>
-  )
+  );
 }
