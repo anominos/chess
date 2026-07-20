b955fa45344780e12615d697b2dc99f967b13bd3
anominos <anominosgamer@gmail.com>
2026-07-20 14:27:31 +0100

    initial frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is working!
+      </h1>
+    </div>
+  )
+}
