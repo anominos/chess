dc862df7f458a226771001e51947e0d92cc8773a
anominos <anominosgamer@gmail.com>
2026-09-04 18:07:38 +0100

    refactor check magic

--- backend/src/bin/gen_magic.rs
+++ backend/src/bin/gen_magic.rs
@@ -1,5 +1,5 @@
 use backend::consts::{BISHOP_RELEVANCY, ROOK_RELEVANCY};
-use std::collections::{HashMap, hash_map::Entry};
+use std::{collections::HashMap, sync::LazyLock};
 
 const ROOK_DIRS: [(i32, i32); 4] = [
     (1, 0),  // north
@@ -14,43 +14,63 @@ const BISHOP_DIRS: [(i32, i32); 4] = [
     (-1, 1),  // SE
     (-1, -1), // SW
 ];
+
+const MAX_SIZE: usize = 16384;
+
+// Tuple of (blocker, attacks) for each square
+static BISHOP_BLOCKER_ATTACKS: LazyLock<[Vec<(u64, u64)>; 64]> = LazyLock::new(|| {
+    std::array::from_fn(|square| {
+        let relevancy = BISHOP_RELEVANCY[square];
+        let mut blockers = relevancy;
+        (0..1usize << relevancy.count_ones())
+            .map(|_| {
+                blockers = blockers.wrapping_sub(1) & relevancy;
+                let attacks = calculate_attacks(square, blockers, &BISHOP_DIRS);
+                (blockers, attacks)
+            })
+            .collect()
+    })
+});
+
+static ROOK_BLOCKER_ATTACKS: LazyLock<[Vec<(u64, u64)>; 64]> = LazyLock::new(|| {
+    std::array::from_fn(|square| {
+        let relevancy = ROOK_RELEVANCY[square];
+        let mut blockers = relevancy;
+        (0..1usize << relevancy.count_ones())
+            .map(|_| {
+                blockers = blockers.wrapping_sub(1) & relevancy;
+                let attacks = calculate_attacks(square, blockers, &ROOK_DIRS);
+                (blockers, attacks)
+            })
+            .collect()
+    })
+});
+
 fn main() {}
 
-fn check_magic(magic: u64, shift: u32, is_rook: bool) -> Option<HashMap<u64, u64>> {
-    let mut hash_to_atk: HashMap<u64, u64> = HashMap::with_capacity(16384);
-    let relevancies = if is_rook {
-        ROOK_RELEVANCY
+fn check_magic(magic: u64, shift: u32, is_rook: bool) -> Option<[Option<u64>; MAX_SIZE]> {
+    let blocker_attacks = if is_rook {
+        &*ROOK_BLOCKER_ATTACKS
     } else {
-        BISHOP_RELEVANCY
+        &*BISHOP_BLOCKER_ATTACKS
     };
-    let calc_dir = if is_rook { ROOK_DIRS } else { BISHOP_DIRS };
+    let mut array: [Option<u64>; MAX_SIZE] = [None; MAX_SIZE];
     for square in 0..64 {
-        let relevancy = relevancies[square];
-        let mut blockers = relevancy;
-        loop {
-            let hash = blockers.wrapping_mul(magic) >> shift;
-            let attackers = calculate_attackers(square, blockers, &calc_dir);
-
-            match hash_to_atk.entry(hash) {
-                Entry::Vacant(e) => {
-                    e.insert(attackers);
-                }
-                Entry::Occupied(e) if *e.get() != attackers => {
+        for (blocker, attack) in &blocker_attacks[square] {
+            let hash = (blocker.wrapping_mul(magic) >> shift) as usize;
+            if let Some(other_attack) = array[hash] {
+                if other_attack != *attack {
                     return None;
                 }
-                Entry::Occupied(_) => {}
-            };
-
-            if blockers == 0 {
-                break;
+            } else {
+                array[hash] = Some(*attack);
             }
-            blockers = (blockers - 1) & relevancy;
         }
     }
-    Some(hash_to_atk)
+    Some(array)
 }
 
-fn calculate_attackers(square: usize, blockers: u64, directions: &[(i32, i32)]) -> u64 {
+fn calculate_attacks(square: usize, blockers: u64, directions: &[(i32, i32)]) -> u64 {
     let mut attacks = 0;
 
     let rank = (square / 8) as i32;
@@ -76,19 +96,17 @@ fn calculate_attackers(square: usize, blockers: u64, directions: &[(i32, i32)]) 
 
     attacks
 }
-/*
-m = gen random number
-d = {}
-For each piece (0..64)
-    get relevancy
-        n = num bits in relevancy
-        get combination for relevancy (2^n) -> 2^14
-            for each comb:
-            - calculate attack set
-            - d[attack set] -> [hash = comb * m >> 11/13]
 
+fn compress_magic(magic: u64, shift: u32, is_rook: bool, map: &HashMap<u64, u64>) -> u32 {
+    // Given a hashmap and a shift, check if we can increase the shift to reduce highest number in map
+    // check if 1 rshift on all keys in map results in a valid map
+    // valid = either shift into empty key or into key where val are same
+    loop {
+        try_rshift_map(&map);
+    }
+    0
+}
 
-
-
-
-*/
+fn check_rshift_map(map: &HashMap<u64, u64>) -> Result<(), ()> {
+    Ok(())
+}
