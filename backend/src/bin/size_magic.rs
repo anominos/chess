e0db7809e6560d3e8d833c669be156ce21c7b360
anominos <anominosgamer@gmail.com>
2026-09-23 10:29:43 +0100

    more info in size_magic

--- backend/src/bin/size_magic.rs
+++ backend/src/bin/size_magic.rs
@@ -11,11 +11,32 @@ fn main() {
     println!(
         "Total: {:.2}kB",
         (calc_size(ROOK_MAGICS) + calc_size(BISHOP_MAGICS)) as f64 / 1024.0
-    )
+    );
+    println!("----");
+    print_popcounts();
 }
 
 fn calc_size(arr: [magics::Magic; 64]) -> usize {
     arr.iter()
         .map(|m| std::mem::size_of::<magics::Magic>() + std::mem::size_of_val(m.array))
         .sum::<usize>()
+}
+
+fn print_popcounts() -> () {
+    println!("B:");
+    for i in 0..64 {
+        let pc = BISHOP_MAGICS[i].magic.count_ones();
+        print!("{:0>2}({: >4}) ", pc, BISHOP_MAGICS[i].array.len());
+        if i % 8 == 7 {
+            println!();
+        }
+    }
+    println!("R:");
+    for i in 0..64 {
+        let pc = ROOK_MAGICS[i].magic.count_ones();
+        print!("{:0>2}({: >4}) ", pc, ROOK_MAGICS[i].array.len());
+        if i % 8 == 7 {
+            println!();
+        }
+    }
 }
